99144bfda5bbf9403bac92488f15d9466877f05e
trumpton <git@trumpton.uk>
2019-06-08 08:21:23 +0100

    Separate Test Function (Debug Purposes Only)

---
+++ mainwindow_test.cpp
@@ -0,0 +1,15 @@
+//
+// Test Function
+//
+//  This function (with content) should not be uploaded into the repository
+//
+
+#include "mainwindow.h"
+#include "appointment.h"
+#include "contact.h"
+#include "configuration.h"
+
+void MainWindow::on_actionTest_Function_triggered()
+{
+}
+
